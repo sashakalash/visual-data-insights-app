39e24dc7c8255370e859d77c210f2a7df11fc362
Aleksandr Koloshtivin <aleksandr.x.koloshtivin@gsk.com>
2025-01-22 11:52:06 +0100

    Dependencies're added

---
+++ src/app/pages/visual-data-insights/visual-data-insights.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-visual-data-insights',
+  imports: [],
+  templateUrl: './visual-data-insights.component.html',
+  styleUrl: './visual-data-insights.component.scss'
+})
+export class VisualDataInsightsComponent {
+
+}
